414af4014e23578cfe1581eed918ec738e773cf0
Hazim <hazimhafizalshah@outlook.com>
2024-10-13 17:52:51 +0800

    feat: create Square and Board components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+function Square() {
+  return <button className="square">1</button>;
+}
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
+export default function Game() {
   return (
     <div>
-      <button className="square">X</button>
+      <Board />
     </div>
   );
 }
